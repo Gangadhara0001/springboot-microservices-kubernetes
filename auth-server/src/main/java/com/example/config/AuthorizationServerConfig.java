d745afa8868bf661988594aa9c18a794a877f198
Gangadhara0001 <161021776+Gangadhara0001@users.noreply.github.com>
2026-06-30 20:29:14 +0530

    Uncomment @Configuration annotation in AuthorizationServerConfig

--- auth-server/src/main/java/com/example/config/AuthorizationServerConfig.java
+++ auth-server/src/main/java/com/example/config/AuthorizationServerConfig.java
@@ -4,7 +4,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.oauth2.server.authorization.settings.AuthorizationServerSettings;
 
-//@Configuration
+@Configuration
 public class AuthorizationServerConfig {
     @Bean
     public AuthorizationServerSettings authorizationServerSettings(){
